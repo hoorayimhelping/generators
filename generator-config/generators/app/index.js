ae8d6f52987f2940c127907f4366e467c1b3c90f
Bucky Schwarz <d.w.schwarz@gmail.com>
2016-01-04 12:43:28 -0500

    created a config generator

---
+++ generator-config/generators/app/index.js
@@ -0,0 +1,43 @@
+'use strict';
+var generators = require('yeoman-generator');
+
+module.exports = generators.Base.extend({
+  constructor: function () {
+    generators.Base.apply(this, arguments);
+
+    this.option('skip-install', {
+      desc: 'Skips running npm install after things have been generated',
+      defaults: false
+    });
+
+    this.option('skip-install-message', {
+      desc: 'The Installation message shows the available commands',
+      defaults: false
+    });
+  },
+
+  configuring: function () {
+    this.config.save();
+  },
+
+  writing: function () {
+    this.template('config/gitignore', '.gitignore');
+    this.template('config/editorconfig', '.editorconfig');
+    this.template('config/eslintrc', '.eslintrc');
+  },
+
+  installing: function() {
+    if (this.options['skip-install']) {
+      return;
+    }
+
+    this.installDependencies({
+      bower: false,
+      npm: true
+    });
+  },
+
+  end: function() {
+    this.log("Installation has finished!");
+  }
+});
